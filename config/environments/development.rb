4ba38d4dceca45fc89376bb5876d2fc6b61e6440
Victor Borshchov <victor.borshchov@gmail.com>
2015-09-22 16:24:06 +0300

    config production and development for emails delivery

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,6 +13,9 @@ Rails.application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
+  # change to true to allow email to be sent during development
+  config.action_mailer.perform_deliveries = true
+  
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
@@ -38,6 +41,6 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
-  
+
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
