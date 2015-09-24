8a4db2bd93d13b1134bad843096783a8afd403f6
Victor Borshchov <victor.borshchov@gmail.com>
2015-09-24 11:33:23 +0300

    change menu priority

--- app/admin/category.rb
+++ app/admin/category.rb
@@ -1,5 +1,5 @@
 ActiveAdmin.register Category do
-  menu priority: 1
+  menu priority: 2
 
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
