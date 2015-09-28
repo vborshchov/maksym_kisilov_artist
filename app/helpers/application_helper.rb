12b8bfe16b3bf4fb3823970fb89091eb9c867cbe
Victor Borshchov <victor.borshchov@gmail.com>
2015-09-28 21:45:42 +0300

    add "about" helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,12 +5,21 @@ module ApplicationHelper
       categories.to_a.map do |category, nested_categories|
         @category = category
         content_tag(:li, (
-          # content_tag(:a, category.name, href: (category.leaf? ? "##" : "#" )) +
           render('layouts/category', locals: {category: @category}) +
           content_tag(:ul, menu_tree_for(nested_categories), class: "dropdown")), class: ("has-dropdown" if nested_categories.size > 0)
         )
       end.join.html_safe
     end
+  end
+
+  def about(artwork)
+    capture = [
+      artwork.name,
+      artwork.dimension,
+      artwork.material,
+      artwork.category.name
+    ]
+    capture.reject(&:blank?).join(", ")
   end
 
 end
