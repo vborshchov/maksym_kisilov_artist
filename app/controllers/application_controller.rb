670cc537de1e9eea60c98860772c2861f7f12559
Victor Borshchov <victor.borshchov@gmail.com>
2015-09-30 09:25:06 +0300

    make showing menu item 'managemet' then admin user signed in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_filter :set_categories
-
+  
   private
     def set_categories
       @categories = Category.hash_tree
