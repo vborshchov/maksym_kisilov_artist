61573605172f3279948c2b86d9f6c082bee6bfb7
Victor Borshchov <victor.borshchov@gmail.com>
2015-10-14 23:44:13 +0300

    make i18n in active admin resourses

--- app/admin/artwork.rb
+++ app/admin/artwork.rb
@@ -1,5 +1,5 @@
 ActiveAdmin.register Artwork do
-  menu priority: 1, label: "Роботи"
+  menu priority: 1
 
   filter :category, label: "Категорії", as: :select, collection: proc { Category.leaves }
   filter :name, label: "Назва"
@@ -7,15 +7,15 @@ ActiveAdmin.register Artwork do
 
   config.clear_action_items!
   action_item "Edit", only:[:show] do
-    link_to "Редагувати", edit_admin_artwork_path
+    link_to t('.artwork.edit.button'), edit_admin_artwork_path
   end
 
   action_item "Delete", only:[:show] do
-    link_to "Видалити", admin_artwork_path, method: "delete", data: {confirm: "Ви впевнені що хочете видалити цю роботу?"}
+    link_to t('.artwork.delete.button'), admin_artwork_path, method: "delete", data: {confirm: t('.artwork.delete.confirm')}
   end
 
   action_item "New", only:[:index] do
-    link_to "Додати роботу", new_admin_artwork_path
+    link_to t('.artwork.new'), new_admin_artwork_path
   end
 
   batch_action "На головну" do |ids|
@@ -46,14 +46,14 @@ ActiveAdmin.register Artwork do
 
   index do
     selectable_column
-    column "Назва", :name
-    column "Позиція", :position
-    column "Категорія", :category_id, :sortable => :category_id do |artwork|
+    column :name
+    column :position
+    column :category_id, :sortable => :category_id do |artwork|
       artwork.category.name
     end
-    column "На говній", :for_main_page
-    column "Розмір", :dimension
-    column "Матеріал", :material
+    column :for_main_page
+    column :dimension
+    column :material
     actions
   end
 
@@ -66,25 +66,25 @@ ActiveAdmin.register Artwork do
   show do
     panel "" do
       table_for artwork do |f|
-        column "Фото" do |image|
+        column " " do |image|
           cl_image_tag(image.picture_url, height: 300, alt: image.name)
         end
       end
     end
   end
 
-  sidebar "Деталі", only: :show do
+  sidebar " ", only: :show do
     attributes_table_for artwork do
-      row "Назва" do |art|
+      row t('.artwork.sidebar.name') do |art|
         art.name
       end
-      row "Розмір" do |art|
+      row t('.artwork.sidebar.dimension') do |art|
         [art.dimension, "см"].reject(&:blank?).join(" ")
       end
-      row "Матеріал" do |art|
+      row t('.artwork.sidebar.material') do |art|
         art.material
       end
-      row "Категорія" do |art|
+      row t('.artwork.sidebar.category') do |art|
         art.category.name if art.category
       end
     end
@@ -92,15 +92,14 @@ ActiveAdmin.register Artwork do
 
   form do |f|
     f.inputs "Робота", :multipart => true do
-      f.input :name, label: "Назва"
-      f.input :dimension, label: "Розмір"
-      f.input :material, as: :select, :collection => %w(папір холст), label: "Матеріал"
-      f.input :picture, :as => :file, :hint => image_tag(f.object.picture.url, width: 150, height: 150, crop: :fit), label: "Фото"
-      f.input :remote_picture_url, label: "або ссилка на фото"
+      f.input :name
+      f.input :dimension
+      f.input :material, as: :select, :collection => %w(папір холст)
+      f.input :picture, :as => :file, :hint => image_tag(f.object.picture.url, width: 150, height: 150, crop: :fit)
+      f.input :remote_picture_url
       f.input :picture_cache, :as => :hidden
-      f.input :category, as: :select, :collection => Category.leaves, label: "Категорія"
-      f.input :for_main_page, as: :radio, label: "Для головної сторінки?"
-      # f.input :position, as: :hidden, input_html: { value: Artwork.pluck(:position).max + 10}
+      f.input :category_id, as: :select, :collection => Category.leaves
+      f.input :for_main_page, as: :radio
     end
     f.actions
   end
--- app/admin/category.rb
+++ app/admin/category.rb
@@ -1,5 +1,5 @@
 ActiveAdmin.register Category, :as => "All Category" do
-  menu priority: 2, label: "Категорії"
+  menu priority: 2
 
   around_filter do |controller, action|
       Category.class_eval do
@@ -24,10 +24,23 @@ ActiveAdmin.register Category, :as => "All Category" do
     link_to "Редагувати", edit_admin_all_category_path
   end
   action_item "Delete", only:[:show] do
-    link_to "Видалити", admin_all_category_path, method: "delete", data: {confirm: "Ви впевнені що хочете видалити цю роботу?"}
+    link_to t('.category.delete.button'), admin_all_category_path, method: "delete", data: {confirm: t('.category.delete.confirm')}
   end
   action_item "New", only:[:index] do
-    link_to "Додати категорію", new_admin_all_category_path
+    link_to t('.category.new'), new_admin_all_category_path
+  end
+
+  index do
+    selectable_column
+    column "Назва", :name
+    column "Батьківська категорія", :parent_id, :sortable => :parent_id do |category|
+      category.parent.name if category.parent
+    end
+    actions
+  end
+
+  sidebar "Графік", only: :index do
+    # graph of caterogies hierarchy goes here
   end
 
   form do |f|
