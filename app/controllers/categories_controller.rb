b46add63c19bfc3cdca9e8a213c98a8eebd01220
Victor Borshchov <victor.borshchov@gmail.com>
2015-10-26 22:30:40 +0200

    rename application.css.erb to application.scss.erb

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,8 +2,12 @@ class CategoriesController < ApplicationController
   require 'unicode'
 
   def show
-    @category = Category.friendly.find(params[:id])
-    @artworks = Artwork.where(category_id: @category.self_and_descendant_ids).order("position ASC").page(params[:page]).per(params[:per])
+    if params[:id] == "all"
+      @artworks = Artwork.all.order("position ASC").page(params[:page]).per(params[:per])
+    else
+      @category = Category.friendly.find(params[:id])
+      @artworks = Artwork.where(category_id: @category.self_and_descendant_ids).order("position ASC").page(params[:page]).per(params[:per])
+    end
   end
 
   private
