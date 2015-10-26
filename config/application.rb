a6c9a743733f2748021f408ea715e6592e03e3b7
Victor Borshchov <victor.borshchov@gmail.com>
2015-10-26 16:27:34 +0200

    set fallbacks_for_empty_translations to true

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,7 @@ module MaksymkisilovArtist
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.i18n.fallbacks = true
     config.autoload_paths += %W(#{config.root}/app/validators/)
     I18n.available_locales = [:en, :uk, :pl]
     config.i18n.default_locale = :uk
