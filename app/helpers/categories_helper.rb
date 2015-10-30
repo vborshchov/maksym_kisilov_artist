c5be2125ae46216087c4c8c6d244739c6e895aa8
Victor Borshchov <victor.borshchov@gmail.com>
2015-10-30 15:01:20 +0200

    Makes photoswipe gallery in main page

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -1,11 +1,11 @@
 module CategoriesHelper
 
-  def gallery_for(artworks)
+  def gallery_for(artworks, width=2000, height=200)
     artworks.map do |a|
       catch (:done) do
         if FastImage.size(a.picture.url)
           "<figure data-pos='" + a.position.to_s + "' itemprop='associatedMedia' itemscope itemtype='http://schema.org/ImageObject'>" + 
-            content_tag(:a, cl_image_tag(a.picture, width: 2000, height: 200, crop: :fit, "itemprop": "thumbnail"), href: a.picture.url, data: {size: FastImage.size(a.picture.url).join("x")}, itemprop: "contentUrl") + 
+            content_tag(:a, cl_image_tag(a.picture, width: width, height: height, crop: :fit, "itemprop": "thumbnail"), href: a.picture.url, data: {size: FastImage.size(a.picture.url).join("x")}, itemprop: "contentUrl") + 
             "<figcaption itemprop='caption description'>" + about(a) + "</figcaption></figure>"
         else
           throw :done
