27843c1918473c3876ab93cfa47c484dbd4d8667
Victor Borshchov <victor.borshchov@gmail.com>
2015-11-02 22:05:32 +0200

    Limits size of pictures in photoswipe to 1500x1500 px

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -4,8 +4,8 @@ module CategoriesHelper
     artworks.map do |a|
       catch (:done) do
         if FastImage.size(a.picture.url)
-          "<figure data-pos='" + a.position.to_s + "' itemprop='associatedMedia' itemscope itemtype='http://schema.org/ImageObject'>" + 
-            content_tag(:a, cl_image_tag(a.picture, width: width, height: height, crop: :fit, "itemprop": "thumbnail"), href: a.picture.url, data: {size: FastImage.size(a.picture.url).join("x")}, itemprop: "contentUrl", onContextMenu: "return false;") + 
+          "<figure data-pos='" + a.position.to_s + "' itemprop='associatedMedia' itemscope itemtype='http://schema.org/ImageObject'>" +
+            content_tag(:a, cl_image_tag(a.picture, width: width, height: height, crop: :fit, "itemprop": "thumbnail"), href: a.picture.url.gsub(/upload\//, 'upload/c_fit,h_1500,w_1500/'), data: {size: FastImage.size(a.picture.url).join("x")}, itemprop: "contentUrl", onContextMenu: "return false;") +
             "<figcaption itemprop='caption description'>" + about(a) + "</figcaption></figure>"
         else
           throw :done
