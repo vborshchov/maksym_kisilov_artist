6a9abe3373fd32db90f6de08d227c732fbaa9106
Victor Borshchov <victor.borshchov@gmail.com>
2015-11-04 15:29:01 +0200

    Makes thumbs' quality=50 and alt equal to artwork's name

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -5,7 +5,7 @@ module CategoriesHelper
       catch (:done) do
         if FastImage.size(a.picture.url)
           "<figure data-pos='" + a.position.to_s + "' itemprop='associatedMedia' itemscope itemtype='http://schema.org/ImageObject'>" +
-            content_tag(:a, cl_image_tag(a.picture, width: width, height: height, crop: :fit, "itemprop": "thumbnail"), href: a.picture.url.gsub(/upload\//, 'upload/c_fit,h_1500,w_1500/'), data: {size: FastImage.size(a.picture.url).join("x")}, itemprop: "contentUrl", onContextMenu: "return false;") +
+            content_tag(:a, cl_image_tag(a.picture, width: width, height: height, crop: :fit, quality: 50, "itemprop": "thumbnail", alt: a.name), href: a.picture.url.gsub(/upload\//, 'upload/c_fit,h_1500,w_1500/'), data: {size: FastImage.size(a.picture.url).join("x")}, itemprop: "contentUrl", onContextMenu: "return false;") +
             "<figcaption itemprop='caption description'>" + about(a) + "</figcaption></figure>"
         else
           throw :done
