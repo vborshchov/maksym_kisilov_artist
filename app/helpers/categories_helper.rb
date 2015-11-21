8df5bae2f9b7de3b82958032e20df3680774dd46
Victor Borshchov <victor.borshchov@gmail.com>
2015-11-21 21:56:27 +0200

    Makes new artworks appear on the top of the gallery and fixes sortable function

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -6,7 +6,7 @@ module CategoriesHelper
         p_url = a.picture.url.gsub(/upload\//, 'upload/c_limit,h_1500,w_1500/')
         p_size = FastImage.size(p_url)
         if p_size
-          "<figure data-pos='" + a.position.to_s + "' itemprop='associatedMedia' itemscope itemtype='http://schema.org/ImageObject'>" +
+          "<figure data-pos='" + a.position.to_s.rjust(5, "0") + "' itemprop='associatedMedia' itemscope itemtype='http://schema.org/ImageObject'>" +
             content_tag(:a, cl_image_tag(a.picture, width: width, height: height, crop: :fit, quality: 50, "itemprop": "thumbnail", alt: a.name), href: p_url, data: {size: p_size.join("x")}, itemprop: "contentUrl", onContextMenu: "return false;") +
             "<figcaption itemprop='caption description'>" + about(a) + "</figcaption></figure>"
         else
